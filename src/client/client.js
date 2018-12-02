5fced0b6a30c99753e19ffba6046f8b9e7091f4e
Denis Denes <denes.dennis@gmail.com>
2018-12-02 12:50:37 +0200

    Setup router for client app

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // Starting point for the client app
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import Home     from './components/Home';
+import React             from 'react';
+import ReactDOM          from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import Routes            from './Routes';
 
-ReactDOM.hydrate(<Home/>, document.querySelector('#root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes/>
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
